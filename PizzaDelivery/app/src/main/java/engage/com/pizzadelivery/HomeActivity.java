48ea34408e7ade0f589f2be878c497cd75dfd888
Norton Samuel Augustus Stanley <norton@in.ibm.com>
2017-09-05 19:20:33 +0530

    Changes to adapt to the new idk

--- PizzaDelivery/app/src/main/java/engage/com/pizzadelivery/HomeActivity.java
+++ PizzaDelivery/app/src/main/java/engage/com/pizzadelivery/HomeActivity.java
@@ -2,10 +2,13 @@ package engage.com.pizzadelivery;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.engage.EngageFailResponse;
 import com.engage.EngageResponse;
@@ -16,12 +19,18 @@ import com.ibm.mobilefirstplatform.clientsdk.android.core.api.BMSClient;
 
 public class HomeActivity extends AppCompatActivity {
 
+    Snackbar snackbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         ImageView imageView = (ImageView) findViewById(R.id.imageView);
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.root_layout);
+       // showSnackBar("This is super cool i think", linearLayout);
         inializeEngage();
+
+
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -32,8 +41,27 @@ public class HomeActivity extends AppCompatActivity {
     }
 
 
+
+    public void showSnackBar(String string, LinearLayout linearLayout)
+    {
+       snackbar = Snackbar.make(linearLayout, string,Snackbar.LENGTH_INDEFINITE).
+                setAction("Ok", new View.OnClickListener()
+                {
+                    @Override
+                    public void onClick(View v)
+                    {
+                        snackbar.dismiss();
+                    }
+                });
+        View snackbarView = snackbar.getView();
+        TextView textView = (TextView) snackbarView.findViewById(android.support.design.R.id.snackbar_text);
+        textView.setMaxLines(50);//this is your max line as your want
+        snackbar.show();
+    }
+
     private void inializeEngage(){
-        EngageConfig engageConfig = new EngageConfig(getApplication(), BMSClient.REGION_US_SOUTH,"e63d4d97-5ce6-459e-b0fc-f5fa032e07e6","4a17f904-8e87-4d92-b7a0-342974955710","norton");
+        //EngageConfig engageConfig = new EngageConfig(getApplication(), BMSClient.REGION_US_SOUTH,"e63d4d97-5ce6-459e-b0fc-f5fa032e07e6","4a17f904-8e87-4d92-b7a0-342974955710","norton");
+        EngageConfig engageConfig = new EngageConfig(getApplication(), BMSClient.REGION_US_SOUTH,"8f6a7c1a-18f6-431c-8159-58396b46c160","6ceeffae-ace2-43c7-b070-6c9fd0bf3ffb","norton-new");
         EngageClient.getInstance().initialize(engageConfig, new EngageResponseListener() {
             @Override
             public void onSuccess(EngageResponse engageResponse) {
