b13bc50bd9e68a2a16ef9f4a9bd89469388771f3
Norton Samuel Augustus Stanley <norton@in.ibm.com>
2017-09-19 18:48:45 +0530

    Changes with the latest idk

---
+++ PizzaDelivery/app/src/main/java/engage/com/pizzadelivery/GetBitmapImage.java
@@ -0,0 +1,33 @@
+package engage.com.pizzadelivery;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Created by norton on 9/16/17.
+ */
+
+public class GetBitmapImage extends AsyncTask<String, Void, Bitmap> {
+
+    protected Bitmap doInBackground(String... urls) {
+        try {
+            URL url = new URL(urls[0]);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setDoInput(true);
+            connection.connect();
+            InputStream input = connection.getInputStream();
+            Bitmap myBitmap = BitmapFactory.decodeStream(input);
+            return myBitmap;
+        } catch (Exception e) {
+            e.getMessage();
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
